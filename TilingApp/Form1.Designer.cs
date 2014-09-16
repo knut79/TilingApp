5f1af140aba4656d9d6cd8424c3ee1e5b969ca9a
knuthp <knut.dullum@gmail.com>
2014-09-16 13:45:32 +0200

    Initial commit

---
+++ TilingApp/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace TilingApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowseFile = new System.Windows.Forms.Button();
+            this.textBoxPrefix = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBrowseFile
+            // 
+            this.btnBrowseFile.Location = new System.Drawing.Point(263, 80);
+            this.btnBrowseFile.Name = "btnBrowseFile";
+            this.btnBrowseFile.Size = new System.Drawing.Size(150, 52);
+            this.btnBrowseFile.TabIndex = 0;
+            this.btnBrowseFile.Text = "Browse for file";
+            this.btnBrowseFile.UseVisualStyleBackColor = true;
+            this.btnBrowseFile.Click += new System.EventHandler(this.btnBrowseFile_Click);
+            // 
+            // textBoxPrefix
+            // 
+            this.textBoxPrefix.Location = new System.Drawing.Point(281, 202);
+            this.textBoxPrefix.Name = "textBoxPrefix";
+            this.textBoxPrefix.Size = new System.Drawing.Size(202, 20);
+            this.textBoxPrefix.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(278, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Prefix";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(278, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "width:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(278, 292);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "height:";
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(319, 263);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWidth.TabIndex = 5;
+            this.textBoxWidth.TextChanged += new System.EventHandler(this.textBoxWidth_TextChanged);
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(319, 289);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHeight.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 431);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxPrefix);
+            this.Controls.Add(this.btnBrowseFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowseFile;
+        private System.Windows.Forms.TextBox textBoxPrefix;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.TextBox textBoxHeight;
+    }
+}
+
---
+++ TilingApp/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Windows;
+
+namespace TilingApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowseFile_Click(object sender, EventArgs e)
+        {
+            int tilewidth = Convert.ToInt32(this.textBoxWidth.Text);
+            tilewidth = tilewidth <= 0 ? 256 : tilewidth;
+
+            var tileheight = Convert.ToInt32(this.textBoxHeight.Text);
+            tileheight = tileheight <= 0 ? 256 : tileheight;
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "all files | *.*"; // file types, that will be allowed to upload
+            dialog.Multiselect = false; // allow/deny user to upload more than one file at a time
+            if (dialog.ShowDialog() == DialogResult.OK) // if user clicked OK
+            {
+                String path = dialog.FileName; // get name of file
+                //using (StreamReader reader = new StreamReader(new FileStream(path, FileMode.Open), new UTF8Encoding())) // do anything you want, e.g. read it
+                //{
+                //}
+
+                //var mainImage = System.Windows.Application.GetResourceStream(new Uri("MainImage.jpg", UriKind.Relative)).Stream;
+                //WriteableBitmap mainWb = new WriteableBitmap(480, 800, 96,
+                //96,
+                //System.Windows.Media.PixelFormats.Bgr32,
+                //null);
+                //var bytesPrPixel = mainWb.Format.BitsPerPixel / 8;
+                //System.Windows.Int32Rect rect = new System.Windows.Int32Rect(0,0,width,height);
+                //var stride = width * bytesPrPixel;
+                ////var pixelBuffer = new Byte(){};
+                //var bytes = new byte[bytesPrPixel];
+                //mainWb.CopyPixels(rect,bytes, bytesPrPixel, stride);
+
+                Bitmap source = new Bitmap(path);
+                var sourceWidth = source.Width;
+                var sourceHeight = source.Height;
+                var horisoltalNumberOfTiles = Math.Ceiling((double)sourceWidth / tilewidth);
+                var verticalNumberOfTiles = Math.Ceiling((double)sourceHeight/tileheight);
+                int remainderWidth = sourceWidth % tilewidth;
+                int remainderHeight = sourceHeight % tileheight;
+                for(int x = 0 ; x < horisoltalNumberOfTiles;x++ )
+                {
+                    var calculatedTileWidth = x  +1 == horisoltalNumberOfTiles ? tilewidth - remainderWidth : tilewidth;
+                    for (int y = 0; y < verticalNumberOfTiles; y++)
+                    {
+                        var calculatedTileHeight = y + 1 == verticalNumberOfTiles ? tileheight - remainderHeight: tileheight; 
+                        Rectangle section = new Rectangle(new System.Drawing.Point(x * tilewidth, y*tileheight), 
+                            new System.Drawing.Size(calculatedTileWidth, calculatedTileHeight));
+                        Bitmap croppedImage = CropImage(source, section);
+                        var outputPath = string.Format("{0}_{1}_{2}.png", this.textBoxPrefix.Text, x, y);
+                        croppedImage.Save(outputPath, System.Drawing.Imaging.ImageFormat.Png);
+                    }
+                }
+            }
+
+            //Bitmap.Clone(Rectangle, PixelFormat) 
+        }
+
+        public Bitmap CropImage(Bitmap source, Rectangle section)
+        {
+            // An empty bitmap which will hold the cropped image
+            Bitmap bmp = new Bitmap(section.Width, section.Height);
+
+            Graphics g = Graphics.FromImage(bmp);
+
+            // Draw the given area (section) of the source image
+            // at location 0,0 on the empty bitmap (bmp)
+            g.DrawImage(source, 0, 0, section, GraphicsUnit.Pixel);
+
+            return bmp;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxWidth_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
